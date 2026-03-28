1c3b4b3465722385296d8042e12a9e53dbee2c6f
Shlok Dwivedi <shlokdwivedi.ngp@gmail.com>
2026-03-28 23:29:43 +0530

    Adjust background color opacity in EventCard

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -57,7 +57,7 @@ export default function EventCard({ event, compact = false, className }: EventCa
         style={{
           background: firstPhoto
             ? undefined
-            : `${accentColor}40`,
+            : `${accentColor}25`,
         }}
       >
         {firstPhoto ? (
