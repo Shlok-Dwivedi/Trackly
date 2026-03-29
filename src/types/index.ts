dd4d5eba3d39bc24507ff60016d0dffdd59e5b72
Shlok Dwivedi <shlokdwivedi.ngp@gmail.com>
2026-03-29 14:45:52 +0530

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -63,6 +63,7 @@ export interface FirestoreEvent {
   googleCalendarEventId?: string;
   coverPhotoUrl?: string;
   coverPosition?: string;
+  coverZoom?: number;
   // Enrollment fields
   enrollmentType?: EnrollmentType;
   capacity?: number;
